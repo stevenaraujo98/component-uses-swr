f67731a88275ea51c2312c9624887fc366b812c1
Steven Araujo <steven.araujo@hotmail.com>
2020-10-07 16:06:27 -0500

    Use Swr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useSWR from 'swr';
+
+function fetcher(url) {
+  return fetch(url).then( res => res.json());
+}
+
+function Pokemon({name}){
+  //swr va a buscar en cache si es algo que ya se habia cargado y mientras
+  //devuelve lo que hay en cache en caso de haber consulta al api nuevamente
+  //para verificar exitiera actualización.
+  const { data } = useSWR(
+    `https://pokeapi.co/api/v2/pokemon/${name}`, 
+    fetcher,
+    { suspense: true } //esto en caso de usar suspende solamente
+  );
+
+  if(!data) return <p>Loading...</p>
+  
+  return <div>
+    <h3>{data.name}</h3>
+    <img src={data.sprites.front_default} alt="Imagen del pokemon :("/>
+  </div>
+}
 
 function App() {
+  const [ name, setName ] = React.useState("pikachu");
+  const [ realName, setRealName ] = React.useState("pikachu");
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setRealName(name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Peticion al Pokiapi</h1>
+      
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={ e => setName(e.target.value)}/>
+        <br/>
+        <button type="submit">Search</button>
+      </form>
+
+      <React.Suspense fallback={<p>Loading {realName}</p>}>{/* Por el suspense: true */}
+        <Pokemon name={realName}/>
+      </React.Suspense>
+
     </div>
   );
 }
